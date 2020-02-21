95927ec309052a62171e94181a68070b51b81d3f
Artem Valkov <valkov.artyom@gmail.com>
2020-02-21 18:02:39 +0200

    fix

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -24,7 +24,7 @@ module.exports = {
     output: {
         filename: `${PATHS.assets}js/[name].js`,
         path: PATHS.dist,
-        publicPath: "/"
+        publicPath: "./"
     },
     optimization: {
         splitChunks: {
